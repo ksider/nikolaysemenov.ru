6d704ef932d81945041b1dd89b669bd6bf8689ea
Nikolay Semenov <semenov.ne@gmail.com>
2022-03-28 15:37:30 +0300

    new

---
+++ script.js
@@ -0,0 +1,44 @@
+
+$(function () {
+  ///// Language Switching (2 languages: English and Chinese). /////
+
+  // Initially disable language switching button.
+  $('#switch-lang').css({'pointer-events':'none',
+   'cursor':'default'}).attr('disabled','disabled');
+
+  function langButtonListen() {
+    $('#switch-lang').click(function (event) {
+      event.preventDefault();
+      $('[lang="ru"]').toggle();
+      $('[lang="en"]').toggle();
+      // Switch cookie stored language.
+      if ($.cookie('lang') === 'en') {
+        $.cookie('lang', 'zh', { expires: 7 });
+      } else {
+        $.cookie('lang', 'en', { expires: 7 });
+      }
+    });
+    // Enable lang switching button.
+    $('#switch-lang').css({'pointer-events':'auto',
+     'cursor':'pointer'}).removeAttr('disabled');
+  }
+
+  // Check if language cookie already exists.
+  if ($.cookie('lang')) {
+    var lang = $.cookie('lang');
+    if (lang === 'en') {
+      $('[lang="ru"]').hide();
+      langButtonListen();
+    } else {
+      $('[lang="en"]').hide();
+      langButtonListen();
+    }
+  } else {
+        
+      $('[lang="ru"]').hide();
+      $.cookie('lang', 'en', { expires: 7 });
+      langButtonListen();
+    
+  }
+});
+
